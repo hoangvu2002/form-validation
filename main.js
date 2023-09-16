ddd7cf3c82ffeffb1357c06c8288fd645c21fb61
pdhoangvu <pdhoangvu@gmail.com>
2023-09-17 00:48:45 +0700

    Add validation for zipcode and country

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line no-unused-vars
 const form = document.querySelector("form");
 
+// eslint-disable-next-line spaced-comment
 //Add inline validation for email
 const email = document.getElementById("email");
 const emailError = document.querySelector("#email + span.error");
 
 
+// eslint-disable-next-line no-unused-vars
 email.addEventListener("input", (event) => {
     // Each time the user types something, we check if the
     // form fields are valid.
@@ -38,3 +42,54 @@ email.addEventListener("input", (event) => {
     // Set the styling appropriately
     emailError.className = "error active";
   }
+
+  // Add validation for country and zipcode input
+  function checkZIP() {
+    // For each country, defines the pattern that the ZIP has to follow
+    const constraints = {
+      ch: [
+        "^(CH-)?\\d{4}$",
+        "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+      ],
+      fr: [
+        "^(F-)?\\d{5}$",
+        "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+      ],
+      de: [
+        "^(D-)?\\d{5}$",
+        "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+      ],
+      nl: [
+        "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+        "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+      ],
+    };
+
+    const country = document.getElementById("Country").value;
+
+    // Get the NPA field
+    const ZIPField = document.getElementById("ZIP");
+    const zipError = document.querySelector("#ZIP + span.error");
+    // Build the constraint checker
+    const constraint = new RegExp(constraints[country][0], "");
+    console.log(constraint);
+  
+    // Check it!
+    if (constraint.test(ZIPField.value)) {
+      // The ZIP follows the constraint, we use the ConstraintAPI to tell it
+      ZIPField.setCustomValidity("");
+      zipError.textContent = "";
+      zipError.className = "error";
+    } else {
+      // The ZIP doesn't follow the constraint, we use the ConstraintAPI to
+      // give a message about the format required for this country
+      ZIPField.setCustomValidity(constraints[country][1]);
+      zipError.textContent = `${constraints[country][1]}`;
+      zipError.className = "error active";
+    }
+  };
+
+  window.onload = () => {
+    document.getElementById("Country").onchange = checkZIP;
+    document.getElementById("ZIP").oninput = checkZIP;
+  };
