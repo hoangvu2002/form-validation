90c278a2f8e5e9a1045653a0af0a32eef964819f
pdhoangvu <pdhoangvu@gmail.com>
2023-09-17 00:07:28 +0700

    Add JS validation for email input

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const form = document.querySelector("form");
 
 //Add inline validation for email
-const email = document.getElementById("mail");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
 
 email.addEventListener("input", (event) => {
     // Each time the user types something, we check if the
@@ -17,3 +19,22 @@ email.addEventListener("input", (event) => {
       showError();
     }
   });
+
+  function showError() {
+    if (email.validity.valueMissing) {
+      // If the field is empty,
+      // display the following error message.
+      emailError.textContent = "You need to enter an email address.";
+    } else if (email.validity.typeMismatch) {
+      // If the field doesn't contain an email address,
+      // display the following error message.
+      emailError.textContent = "Entered value needs to be an email address.";
+    } else if (email.validity.tooShort) {
+      // If the data is too short,
+      // display the following error message.
+      emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    }
+  
+    // Set the styling appropriately
+    emailError.className = "error active";
+  }
